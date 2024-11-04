f277797052d3ff7aee2930ef71b4a649d84dc4b5
roi yotvat <roiyot261098@gmail.com>
2024-11-04 20:52:18 +0200

    fixed bugs on task details

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -31,7 +31,6 @@ export function TaskDetails() {
                     <button onClick={() => navigate(`/`)}>{t("back")}</button>
                 </div>
             </div>
-            <SpaceSvg isNoResults={false} />
         </>
     );
 };
