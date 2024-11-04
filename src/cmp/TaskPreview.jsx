89111c22b9d72418b7a70dea97ce5ec2e866f467
roi yotvat <roiyot261098@gmail.com>
2024-11-04 16:03:54 +0200

    fix bugs and add i18n

--- src/cmp/TaskPreview.jsx
+++ src/cmp/TaskPreview.jsx
@@ -1,21 +1,22 @@
 import { useNavigate } from "react-router-dom"
 import { utilService } from "../services/util.service"
+import { useTranslation } from "react-i18next"
 export function TaskPreview({ task, deleteTask }) {
     const navigate = useNavigate()
-
+    const { t } = useTranslation()
 
     return (
         <div className={`task-preview ${utilService.getPriorityClass(task.priority)}`} onClick={() => navigate(`/task/${task._id}`)} >
             <li >
                 <h2>{task.title}</h2>
                 <p className="task-preview-description">{task.description}</p>
-                <p className="task-preview-prioriy">Priority: {task.priority}</p>
+                <p className="task-preview-prioriy">{t("priority")}: {task.priority}</p>
                 <div className="button-group">
-                    <button onClick={(ev) => deleteTask(ev, task._id)} >Delete</button>
+                    <button onClick={(ev) => deleteTask(ev, task._id)} >{t("deleteLow")}</button>
                     <button onClick={(ev) => {
                         ev.stopPropagation()
                         navigate(`/task/edit/${task._id}`)
-                    }}>Edit</button>
+                    }}>{t("editLow")}</button>
                 </div>
             </li>
         </div>
